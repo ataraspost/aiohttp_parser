0c889bfb8478e47df774dc628f8dc473ccf13dec
ataraspost <ataraspost@gmail.com>
2019-09-04 09:29:44 +0700

    create parser

--- app.py
+++ app.py
@@ -8,12 +8,15 @@ event_loop = asyncio.get_event_loop()
 
 
 async def handle(request):
-    asyncio.ensure_future(parser(timeout=10), loop=event_loop)
-    text = "Hello, world"
-    return web.Response(text=text)
+    try:
+        data = await request.post()
+        asyncio.ensure_future(parser(**data), loop=event_loop)
+    except:
+        return web.json_response({'status': 'error'})
+    return web.json_response({'status': 'ok'})
 
 app = web.Application()
-app.add_routes([web.get('/', handle), ])
+app.add_routes([web.post('/', handle), ])
 
 web.run_app(app)
 
--- parser.py
+++ parser.py
@@ -1,8 +1,59 @@
 
 import asyncio
+import aiohttp
+from bs4 import BeautifulSoup
+import re
+import requests
+
+HEADERS = {'User-Agent': 'Mozilla/5.0'}
 
 
-async def parser(url=None, timeout=0):
-    print('Running in foo')
-    await asyncio.sleep(timeout)
-    print('Explicit context switch to foo again')
+async def parser(url=None, timeout=0, instance_id=None):
+    await asyncio.sleep(int(timeout))
+    data = await get_result(url)
+    data[instance_id] = instance_id
+    requests.post('http://localhost:8000/result/', data=data)
+
+
+async def get_result(url):
+    response = await get_html(url)
+    soup = BeautifulSoup(response, 'html.parser')
+    data = {}
+
+    try:
+        data['titel'] = soup.title.get_text()
+    except:
+        data['titel'] = 'error'
+
+    try:
+        data['h1'] = list()
+        for item in soup.find_all('h1'):
+            data['h1'].append(item.get_text())
+    except:
+        data['h1'] = 'error'
+
+    try:
+        data['charset'] = get_encoding(soup)
+    except:
+        data['charset'] = 'error'
+
+    return data
+
+async def get_html(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, headers=HEADERS) as resp:
+            return await resp.text()
+
+def get_encoding(soup):
+    encod = soup.meta.get('charset')
+    if encod == None:
+        encod = soup.meta.get('content-type')
+        if encod == None:
+            content = soup.meta.get('content')
+            match = re.search('charset=(.*)', content)
+            if match:
+                encod = match.group(1)
+            else:
+                raise ValueError('unable to find encoding')
+    return encod
+
